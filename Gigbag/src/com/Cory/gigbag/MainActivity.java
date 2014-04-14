af38bdd516449b7a54408dd9a6f23859978b76c2
greenmachine1 <scary_penguins05@hotmail.com>
2014-04-14 15:35:56 -0700

    creating the new account activity

--- Gigbag/src/com/Cory/gigbag/MainActivity.java
+++ Gigbag/src/com/Cory/gigbag/MainActivity.java
@@ -1,15 +1,73 @@
 package com.Cory.gigbag;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends Activity {
+	
+	EditText userName;
+	EditText password;
+	
+	Button logInButton;
+	Button createNewAccountButton;
+	
+	Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        context = this;
+        
+        // **** pinpointing the username and passwords **** //
+        userName = (EditText)findViewById(R.id.user_name_edit_text);
+        password = (EditText)findViewById(R.id.password_edit_text);
+        
+        
+        // **** the logInButtons onclicklistener **** //
+        logInButton = (Button)findViewById(R.id.button1);
+        logInButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Log.i("Log in clicked", "Yes");
+				
+			}
+        	
+        });
+        
+        // **** the createAccountButtons onclick listener **** //
+        createNewAccountButton = (Button)findViewById(R.id.button2);
+        createNewAccountButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Log.i("create new account clicked", "Yes");
+				
+				// **** launching the new account activity **** //
+				Intent new_account_intent = new Intent(context, New_Account.class);
+				
+				startActivity(new_account_intent);
+				
+			}
+        	
+        });
+        
+        
     }
 
 
---
+++ Gigbag/src/com/Cory/gigbag/New_Account.java
@@ -0,0 +1,23 @@
+package com.Cory.gigbag;
+
+import com.Cory.gigbag.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class New_Account extends Activity{
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_account);
+        
+        
+        
+        
+        
+        
+        
+	}
+	
+}
