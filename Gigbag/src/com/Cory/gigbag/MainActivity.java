a971f67f001af5fd53abdd539c940f47cf2a1254
greenmachine1 <scary_penguins05@hotmail.com>
2014-04-19 02:42:49 -0700

    working on my connectivity for android

--- Gigbag/src/com/Cory/gigbag/MainActivity.java
+++ Gigbag/src/com/Cory/gigbag/MainActivity.java
@@ -5,6 +5,8 @@ package com.Cory.gigbag;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,9 +14,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
@@ -24,10 +26,15 @@ public class MainActivity extends Activity {
 	EditText userName;
 	EditText password;
 	
+	TextView userNameTextView;
+	TextView passwordTextView;
+	
 	Button logInButton;
 	Button createNewAccountButton;
 	
 	Context context;
+	
+	ConnectivityManager cm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +43,48 @@ public class MainActivity extends Activity {
         
         context = this;
 
+        // **** gathering connection status **** //
+        cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
         
         // **** pinpointing the username and passwords **** //
         userName = (EditText)findViewById(R.id.user_name_edit_text);
         password = (EditText)findViewById(R.id.password_edit_text);
         
+        userNameTextView = (TextView)findViewById(R.id.user_name_text);
+        passwordTextView = (TextView)findViewById(R.id.password_text);
+        
+        logInButton = (Button)findViewById(R.id.button1);
+        createNewAccountButton = (Button)findViewById(R.id.button2);
+        
+        if(isConnected == true){
+        	
+        	userNameTextView.setVisibility(View.VISIBLE);
+        	passwordTextView.setVisibility(View.VISIBLE);
+        	userName.setVisibility(View.VISIBLE);
+        	password.setVisibility(View.VISIBLE);
+        	logInButton.setVisibility(View.VISIBLE);
+        	createNewAccountButton.setVisibility(View.VISIBLE);
+        	
+        }else{
+        	
+        	userNameTextView.setVisibility(View.INVISIBLE);
+        	passwordTextView.setVisibility(View.INVISIBLE);
+        	userName.setVisibility(View.INVISIBLE);
+        	password.setVisibility(View.INVISIBLE);
+        	logInButton.setVisibility(View.INVISIBLE);
+        	createNewAccountButton.setVisibility(View.INVISIBLE);
+        	
+        	
+        }
+        
+        
         
         // **** checking to see if there is a user still **** //
         // **** logged in **** //
         ParseUser currentUser = ParseUser.getCurrentUser();
-        if(currentUser != null){
+        if((currentUser != null) && (isConnected == true)){
         	
         	// **** starting the user info activity **** //
         	Intent userInfoIntent = new Intent(context, User_Info.class);
@@ -58,12 +97,11 @@ public class MainActivity extends Activity {
         
         
         // **** the logInButtons onclicklistener **** //
-        logInButton = (Button)findViewById(R.id.button1);
         logInButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				
 				
 				// **** getting the user info **** //
 				String userNameString = userName.getText().toString();
@@ -97,7 +135,7 @@ public class MainActivity extends Activity {
         });
         
         // **** the createAccountButtons onclick listener **** //
-        createNewAccountButton = (Button)findViewById(R.id.button2);
+        
         createNewAccountButton.setOnClickListener(new OnClickListener(){
 
 			@Override
