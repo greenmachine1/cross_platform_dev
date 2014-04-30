8afa15bcc8933dad5f5be73dfe0a5014ca10e749
greenmachine1 <scary_penguins05@hotmail.com>
2014-04-30 11:59:15 -0700

    added my js files for cloud

---
+++ GigBagCloudCode/cloud/main.js
@@ -0,0 +1,57 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("This is JavaScript code that gets run on the cloud");
+});
+
+
+
+
+Parse.Cloud.afterSave("Post", function(request){
+	
+	var name = request.object.get('bandName');
+
+	var pushQuery = new Parse.Query(Parse.Installation);
+	pushQuery.equalTo('deviceType', 'ios');
+	
+	Parse.Push.send({
+		where: pushQuery,
+		data: {
+			alert: "" + name
+		}
+	},{
+	sucess: function(){
+		// push was successful
+	},
+	error: function(error){
+		throw "Got an error " + error.code + " : " + error.message;
+	}
+	});
+	
+});
+
+
+
+
+
+
+
+Parse.Cloud.afterDelete("Post", function(request){
+
+	var pushQuery = new Parse.Query(Parse.Installation);
+	pushQuery.equalTo('deviceType', 'ios');
+	
+	Parse.Push.send({
+		where: pushQuery,
+		data: {
+		}
+	},{
+	sucess: function(){
+		// push was successful
+	},
+	error: function(error){
+		throw "Got an error " + error.code + " : " + error.message;
+	}
+	});
+});
